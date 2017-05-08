b38f26ad4e02ce14a61a957bfc2ca6d557cf1f9a
jupiterclapton <jupiterclapton@outlook.com>
2017-05-08 18:23:13 +0000

    added basic apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+package 'apache2' do
+     	 package_name 'apache2'
+     	 action :install
+end
+
+service 'apache2' do
+	service_name 'apache2'
+	action [:start, :enable]
+end
